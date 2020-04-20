8c6b30b5764ebd8630d255036e746d9ce83c0679
Azim0ff <figaRobot@yandex.ru>
2020-04-20 13:12:00 +0300

    at least something is being drawn

--- src/main/java/icu/azim/wagrapple/entity/GrappleLine.java
+++ src/main/java/icu/azim/wagrapple/entity/GrappleLine.java
@@ -1,9 +1,12 @@
 package icu.azim.wagrapple.entity;
 
+import java.util.UUID;
+
 import icu.azim.wagrapple.WAGrappleMod;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
@@ -14,8 +17,10 @@ public class GrappleLine extends Entity {
 	
 
 	private PlayerEntity player;
+	private UUID playerUUID;
 	private Vec3d hitPos;
 	private float length = 0;
+	private int lastId;
 
 	public GrappleLine(EntityType<?> type, World world) {
 		super(type, world);
@@ -24,14 +29,17 @@ public class GrappleLine extends Entity {
 	public GrappleLine(World world, PlayerEntity player, float length, Vec3d pos) {
 		this(WAGrappleMod.GRAPPLE_LINE, world);
 		this.updatePosition(pos.x, pos.y, pos.z);
+		lastId = player.getEntityId();
 		this.hitPos = pos;
 		this.player = player;
 		this.setLength(length);
 		this.ignoreCameraFrustum = true;
+		playerUUID = player.getUuid();
 	}
 
 	@Override
 	protected void initDataTracker() {
+		//FishingBobberEntity
 	}
 
 	@Override
@@ -44,8 +52,17 @@ public class GrappleLine extends Entity {
 
 	@Override
 	public Packet<?> createSpawnPacket() {
-	    return new EntitySpawnS2CPacket(this, this.getEntityId());
+	    return new EntitySpawnS2CPacket(this, player==null?this.getEntityId():player.getEntityId());
 	}
+	
+	@Override
+	public void tick() {
+		if(player==null) {
+			this.remove();
+		}
+		super.tick();
+	}
+	
 	
 	public PlayerEntity getPlayer() {
 		return player;
--- src/main/java/icu/azim/wagrapple/render/GrappleLineRenderer.java
+++ src/main/java/icu/azim/wagrapple/render/GrappleLineRenderer.java
@@ -40,10 +40,6 @@ public class GrappleLineRenderer extends EntityRenderer<GrappleLine>{
 	@Override
 	public void render(GrappleLine entity, float yaw, float tickDelta, MatrixStack matrices,
 			VertexConsumerProvider vertexConsumers, int light) {
-		counter++;
-		if(counter%5==0) {
-			System.out.println(counter);
-		}
 		RenderSystem.pushMatrix();
 		RenderSystem.translated(entity.getX(), entity.getY(), entity.getZ());
 		RenderSystem.enableRescaleNormal();
@@ -58,22 +54,80 @@ public class GrappleLineRenderer extends EntityRenderer<GrappleLine>{
 		Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder buffer = tessellator.getBuffer();
 
+		PlayerEntity entityplayer = entity.getPlayer();
 		
 		//VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getLines());
 		
-		PlayerEntity entityplayer = entity.getPlayer();
 		int k = (entity.getPlayer().getActiveHand()==Hand.MAIN_HAND?1:-1);
-		
+        
+		float swingProgress = entityplayer.getHandSwingProgress(tickDelta);
+		float f8 = MathHelper.sin(MathHelper.sqrt(swingProgress) * (float) Math.PI);
+		float f9 = (entityplayer.prevBodyYaw
+				+ (entityplayer.bodyYaw - entityplayer.prevBodyYaw) * tickDelta) * 0.017453292F;
+		double yawSin = (double) MathHelper.sin(f9);
+		double yawCos = (double) MathHelper.cos(f9);
 		double handAdjustment = (double) k * 0.35D;
-		Vec3d start = entityplayer.getPos().add(new Vec3d(handAdjustment,entityplayer.getEyeHeight(entityplayer.getPose()),0));
+		//double d3 = 0.8D;
+		double playerX;
+		double playerY;
+		double playerZ;
+		double eyeHeightAdjustment;
+
+		if ((this.renderManager.gameOptions == null || this.renderManager.gameOptions.perspective <= 0)
+				/*&& entityplayer == Minecraft.getMinecraft().player*/) {
+			float f10 = (float) this.renderManager.gameOptions.fov;
+			f10 = f10 / 100.0F;
+			Vec3d vec3d = new Vec3d((double) k * -0.36D * (double) f10, -0.045D * (double) f10, 0.4D);
+			vec3d = vec3d.rotateX(-(entityplayer.prevPitch
+					+ (entityplayer.pitch - entityplayer.prevPitch) * tickDelta) * 0.017453292F);
+			vec3d = vec3d.rotateY(-(entityplayer.prevYaw
+					+ (entityplayer.yaw - entityplayer.prevYaw) * tickDelta) * 0.017453292F);
+			vec3d = vec3d.rotateY(f8 * 0.5F);
+			vec3d = vec3d.rotateX(-f8 * 0.7F);
+			playerX = entityplayer.prevX + (entityplayer.getX() - entityplayer.prevX) * (double) tickDelta
+					+ vec3d.x;
+			playerY = entityplayer.prevY + (entityplayer.getY() - entityplayer.prevY) * (double) tickDelta
+					+ vec3d.y;
+			playerZ = entityplayer.prevZ + (entityplayer.getZ() - entityplayer.prevZ) * (double) tickDelta
+					+ vec3d.z;
+			eyeHeightAdjustment = (double) entityplayer.getEyeHeight(entityplayer.getPose());
+		} else {
+			playerX = entityplayer.prevX + (entityplayer.getX() - entityplayer.prevX) * (double) tickDelta
+					- yawCos * handAdjustment - yawSin * 0.8D;
+			playerY = entityplayer.prevY + (double) entityplayer.getEyeHeight(entityplayer.getPose())
+					+ (entityplayer.getX() - entityplayer.prevY) * (double) tickDelta - 0.8D;
+			playerZ = entityplayer.prevZ + (entityplayer.getZ() - entityplayer.prevZ) * (double) tickDelta
+					- yawSin * handAdjustment + yawCos * 0.4D;
+			eyeHeightAdjustment = entityplayer.isSneaking() ? -0.1875D : 0.0D;
+		}
+		double groundX = entity.prevX + (entity.getX() - entity.prevX) * (double) tickDelta;
+		double groundY = entity.prevY + (entity.getY() - entity.prevY) * (double) tickDelta + 0.25D;
+		double grounxZ = entity.prevZ + (entity.getZ() - entity.prevZ) * (double) tickDelta;
+		double deltaX = (double) ((float) (playerX - groundX));
+		double deltaY = (double) ((float) (playerY - groundY)) + eyeHeightAdjustment;
+		double deltaZ = (double) ((float) (playerZ - grounxZ));
+		
+		
+		
+		Vec3d start = entityplayer.getPos();//.add(new Vec3d(handAdjustment,entityplayer.getEyeHeight(entityplayer.getPose()),0));
 		Vec3d offset = entity.getHitPos().subtract(start);
 
 		buffer.begin(1, VertexFormats.POSITION_COLOR);
-		
-		for(int i = 0;i<16;i++) {
-			buffer.vertex(  (start.x+offset.x*(i/16)),  (start.y+offset.y*(i/16)),  (start.z+offset.z*(i/16))).color(254, 254, 254, 254).light(254).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).normal(matrices.peek().getNormal(), 0, 1, 0).next();
-			buffer.vertex(  (start.x+offset.x*((i+1)/16)),  (start.y+offset.y*((i+1)/16)),  (start.z+offset.z*((i+1)/16))).color(254, 254, 254, 254).light(254).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).normal(matrices.peek().getNormal(), 0, 1, 0).next();
+		for (int count = 0; count <= 16; ++count) {
+			float proportion = (float) count / 16.0F;
+			buffer.vertex(entity.getX() + deltaX*proportion,entity.getY()+ deltaY * (double) (proportion * proportion + proportion) * 0.5D, entity.getZ() + deltaZ * (double) proportion)
+			.color(254, 254, 254, 254).light(254).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).normal(matrices.peek().getNormal(), 0, 1, 0).next();
 		}
+		
+		//buffer.vertex(start.x,  start.y,  start.z).color(254, 254, 254, 254).light(254).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).normal(matrices.peek().getNormal(), 0, 1, 0).next();
+		//buffer.vertex(entity.getHitPos().x,  entity.getHitPos().y,  entity.getHitPos().z).color(254, 254, 254, 254).light(254).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).normal(matrices.peek().getNormal(), 0, 1, 0).next();
+		
+		
+		
+		//for(int i = 0;i<16;i++) {
+			//buffer.vertex(  (start.x+offset.x*(i/16)),  (start.y+offset.y*(i/16)),  (start.z+offset.z*(i/16))).color(254, 254, 254, 254).light(254).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).normal(matrices.peek().getNormal(), 0, 1, 0).next();
+			//buffer.vertex(  (start.x+offset.x*((i+1)/16)),  (start.y+offset.y*((i+1)/16)),  (start.z+offset.z*((i+1)/16))).color(254, 254, 254, 254).light(254).texture(1, 1).overlay(OverlayTexture.DEFAULT_UV).normal(matrices.peek().getNormal(), 0, 1, 0).next();
+	//	}
 		tessellator.draw();
 		//matrices.pop();
 		RenderSystem.disableBlend();
